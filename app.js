4d4d8cbb49e22b43ea483be0d5912a90608656ce
Chris Kitenge Mbuyi <mbuyicmk394@gmail.com>
2024-03-24 19:22:55 +0200

    Checking db connection

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URL)
+      console.log(process.env.MONGODB_URL)
     app.listen(port, () => console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error)
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URL;
 
 const connectDB = (url) => {
   return mongoose.connect(url);
